b5317c262b6907728cf14a5032fc210cc503582e
zhihan <zhihanlin29@gmail.com>
2024-03-16 23:04:05 +0100

    changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
+    build: {
+    outDir: 'dist',
+  },
   server: {
     watch: {
       usePolling: true,
